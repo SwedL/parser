048180dc9a04361796b1bb7088048627e7629fd9
autotrade <swedl@yandex.ru>
2023-04-06 18:50:04 +0500

    модуль 'пагинация'

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 from bs4 import BeautifulSoup
+import requests
 
+url = 'https://parsinger.ru/html/index3_page_1.html'
 
+response = requests.get(url=url)
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'lxml')
+shuma = 'https://parsinger.ru/html/'
+pagin = [shuma+t['href'] for t in soup.find('div', 'pagen').find_all('a')]
 
-with open('index.html', 'r', encoding='utf-8') as file:
-    soup = BeautifulSoup(file, 'lxml')
-    print(soup)
+#print(soup)
+
+res_art = []
+
+link_item = []
+for p in pagin:
+    resp = requests.get(url=p)
+    resp.encoding = 'utf-8'
+    s = BeautifulSoup(resp.text, 'lxml')
+    [link_item.append(shuma+link.find('a')['href']) for link in s.find_all('div', class_='img_box')]
+
+#print(link_item)
+
+for item in link_item:
+    resp1 = requests.get(url=item)
+    resp1.encoding = 'utf-8'
+    soup1 = BeautifulSoup(resp1.text, 'lxml')
+    res_art.append(int(soup1.find('p', 'article').text.split()[-1]))
+
+print(sum(res_art))
 
 
 

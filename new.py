df2a718952c85068c5b58cf1178d2b65fc22e0b2
autotrade <swedl@yandex.ru>
2023-04-07 17:21:27 +0500

    модуль 'парсинг таблиц'

--- new.py
+++ new.py
@@ -1,5 +1,26 @@
-url = 'https://openweathermap.org/data/2.5/weather?id=1492663&appid=439d4b804bc8187953eb36d2a8c26a02'
-url2 = 'https://openweathermap.org/data/2.5/onecall?lat=59.6033&lon=60.5787&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02'
+import requests
+from bs4 import BeautifulSoup
 
-url1_M = 'https://openweathermap.org/data/2.5/weather?id=524901&appid=439d4b804bc8187953eb36d2a8c26a02'
-url2_M = 'https://openweathermap.org/data/2.5/onecall?lat=55.7522&lon=37.6156&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02'
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+    'x-requested-with': 'XMLHttpRequest'
+}
+
+url = "https://parsinger.ru/table/5/index.html"
+
+response = requests.get(url=url)
+response.encoding = 'utf-8'
+
+soup = BeautifulSoup(response.text, 'lxml')
+dict1 = {}
+
+res = [[float(i) for i in x.text.split('\n')[1:-1]] for x in soup.find('div', 'main').find_all('tr')[1:]]
+
+print(len(res))
+print(res)
+result = [0] * 15
+
+for k in res:
+    for key in range(1, 16):
+        key_string = f'{key} column'
+        dict1.setdefault(key_string, )
